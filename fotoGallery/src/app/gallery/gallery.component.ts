d43e322e1bcffa718e11c20cbcaadb2ff019e6de
alessiosalzillo <152988228+Alessio2107@users.noreply.github.com>
2024-04-03 09:53:40 +0200

    Add files via upload

---
+++ fotoGallery/src/app/gallery/gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent {
+  images: string[] = [
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.cleanpng.com%2Fpng-wyhpmt%2F&psig=AOvVaw3EMgPrRvZvLqciCZnZHQrV&ust=1710858595621000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCh1ImD_oQDFQAAAAAdAAAAABAE ',
+    ' '
+
+  ];
+
+}
